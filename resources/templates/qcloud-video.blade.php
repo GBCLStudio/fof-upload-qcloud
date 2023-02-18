2d7574877f37ddb1a429b776b653374b5189ecfd
超屑的拜瑞 <71890055+Burial0268@users.noreply.github.com>
2023-02-19 02:10:19 +0800

    Remove Unwanted Code

--- resources/templates/qcloud-video.blade.php
+++ resources/templates/qcloud-video.blade.php
@@ -1,6 +1,5 @@
 <div id="dplayer_{@uuid}" style="height: auto; width: auto"></div>
 
-<script src="https://cdn.jsdelivr.net/npm/dplayer@1.27.0/dist/DPlayer.min.js"></script>
 <script type="text/javascript">
     var dp = new DPlayer({
         container: document.getElementById("dplayer_{@uuid}"),
