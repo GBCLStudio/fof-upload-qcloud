57a2b23ef51de10edc76a655253211334857652f
超屑的拜瑞 <71890055+Burial0268@users.noreply.github.com>
2023-02-19 02:21:44 +0800

    fix/update:Optimize Dependency loading

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,11 +1,32 @@
 import app from 'flarum/forum/app';
 import downloadButtonInteraction from './downloadButtonInteraction';
 
-var head = document.head || document.getElementsByTagName('head')[0];
-var script = document.createElement('script');
-script.setAttribute("src", "https://cdn.jsdelivr.net/npm/dplayer@latest/dist/DPlayer.min.js");
-head.appendChild(script);
-
+function loadScript(url, callback) {
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	if (typeof(callback) != "undefined") {
+		if (script.readyState) {
+			script.onreadystatechange = function() {
+				if (script.readyState == "loaded" || script.readyState == "complete") {
+					script.onreadystatechange = null;
+					callback();
+				}
+			};
+		} else {
+			script.onload = function() {
+				callback();
+			};
+		}
+	};
+	script.src = url;
+	document.body.appendChild(script);
+}
+loadScript("https://cdn.jsdelivr.net/npm/dplayer@latest/dist/DPlayer.min.js", function() {
+    alert('[gbcl/fof-upload-qcloud] Loading dependency "Dplayer" succeeded');
+});
+loadScript("https://cdn.jsdelivr.net/npm/pdfobject@latest/pdfobject.min.js", function() {
+    alert('[gbcl/fof-upload-qcloud] Loading dependency "PDFObject" succeeded');
+});
 app.initializers.add('gbcl-fof-upload-qcloud', () => {
   downloadButtonInteraction();
 });
