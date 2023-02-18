c593cbe98009b15261c303dff03eb075dda58e5f
超屑的拜瑞 <71890055+Burial0268@users.noreply.github.com>
2023-02-19 02:09:25 +0800

    fix: fix "DPlayer is not defined"

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,6 +1,10 @@
 import app from 'flarum/forum/app';
-
 import downloadButtonInteraction from './downloadButtonInteraction';
+
+var head = document.head || document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.setAttribute("src", "https://cdn.jsdelivr.net/npm/dplayer@latest/dist/DPlayer.min.js");
+head.appendChild(script);
 
 app.initializers.add('gbcl-fof-upload-qcloud', () => {
   downloadButtonInteraction();
