eb819f0d856e59f31644062242045be751974b3c
Burial0268 <burial0268@foxmail.com>
2024-08-02 01:36:10 +0800

    chore: something

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,9 +1,17 @@
 import app from "flarum/forum/app";
 import createComponent from "./createComponent";
+import { componentType } from "./createComponent";
 import CommentPost from "flarum/forum/components/CommentPost";
 import {extend} from "flarum/common/extend";
 import downloadButtonInteraction from "./downloadButtonInteraction";
 
+const createComponentIfNeeded = (containers: NodeListOf<Element>, type: componentType) => {
+  for (const container of Array.from(containers)) {
+    if (container.children.length === 0) {
+      createComponent(container as HTMLElement, type);
+    }
+  }
+};
 
 app.initializers.add("gbcl-fof-upload-qcloud", () => {
 downloadButtonInteraction();
@@ -12,18 +20,7 @@ downloadButtonInteraction();
     const playerContainers = document.querySelectorAll(".qcloud-player-container")
     const pdfContainers = document.querySelectorAll('.qcloud-pdf-container');
 
-    if (playerContainers.length) {
-      for (const i of Array.from(playerContainers)) {
-        if (i.children.length) continue;
-        createComponent(i as HTMLElement, 'video')
-      }
-    }
-
-    if (pdfContainers.length) {
-      for (const i of Array.from(pdfContainers)) {
-        if (i.children.length) continue;
-        createComponent(i as HTMLElement, 'pdf')
-      }
-    }
+    createComponentIfNeeded(playerContainers, 'video');
+    createComponentIfNeeded(pdfContainers, 'pdf');
   });
 });
